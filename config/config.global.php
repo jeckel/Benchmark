9a6fefcc7fdc191642d71d230e327fd90f3684ea
Julien <julien@tvty.tv>
2016-04-25 17:01:12 +0200

    Init config

---
+++ config/config.global.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'engine' => [
+        'dflt_nb_lines_to_insert'  => 1000 * 1000,
+        'dflt_nb_lines_offset_log' => 10 * 1000,
+        'log_file_mask'            => 'log_%s.log'
+    ],
+    'tasks' => [
+    ]
+];
